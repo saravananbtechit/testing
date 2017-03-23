daf286535f909f2497cd089e0192efcc38084b92
jp <jp@10.144.158.142>
2017-03-23 18:15:46 +0530

    just check

---
+++ src/main/java/au/vha/myvfgold/DemoFunction.java
@@ -0,0 +1,13 @@
+package au.vha.myvfgold;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class DemoFunction implements RequestHandler<String,String> {
+
+	public String handleRequest(String arg0, Context arg1) {
+		
+		return "Sample lambda....Cool..You are in a right path..just it.";
+	}
+// 966779 - IT for clear space
+}
